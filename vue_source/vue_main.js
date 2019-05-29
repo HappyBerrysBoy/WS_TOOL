cacf90de0ede490c20cbbecad2947334b8fa0ee8
happyberrysboy <elytselyts@gmail.com>
2019-05-29 13:46:00 +0900

    'family_site_bugfix'

--- vue_source/vue_main.js
+++ vue_source/vue_main.js
@@ -1,118 +1,169 @@
-Vue.use(VueMaterial.default)
-
-(new Vue({
+Vue.use(VueMaterial.default)(
+  new Vue({
     el: '#app',
     data: {
-        show: false,
-        menuIcon: 'menu',
-        showUserShortcut: false,
-        showTagShortcut: false,
-        showTagFilter: false,
-        showCategory: false,
-        showGetMarkdown: false,
-        showFamilySite: false,
-        showRunUrl: false,
-        scotVoting:[],
-        tagFilterList: []
+      show: false,
+      menuIcon: 'menu',
+      showUserShortcut: false,
+      showTagShortcut: false,
+      showTagFilter: false,
+      showCategory: false,
+      showGetMarkdown: false,
+      showFamilySite: false,
+      showRunUrl: false,
+      scotVoting: [],
+      tagFilterList: [],
+      steemVP: 0,
+      sctVP: 0,
     },
     created() {
-        this.tagFilterList = JSON.parse(localStorage.getItem(WSTOOLS_POST_FILTER_KEY));
+      this.tagFilterList = JSON.parse(
+        localStorage.getItem(WSTOOLS_POST_FILTER_KEY),
+      );
     },
     mounted() {
-        window.addEventListener("hashchange", () => {
-            console.log(location.hash)
-            this.showCategory = location.hash === "#category"
-        }, false);
+      window.addEventListener(
+        'hashchange',
+        () => {
+          console.log(location.hash);
+          this.showCategory = location.hash === '#category';
+        },
+        false,
+      );
 
-        window.document.body.addEventListener('resize', () => {
-            alert('test');
-        });
+      window.document.body.addEventListener('resize', () => {
+        alert('test');
+      });
 
-        $('.App__content').resize(function() {
-            alert('zdzxc;lksjdf');
-        });
+      $('.App__content').resize(function() {
+        alert('zdzxc;lksjdf');
+      });
 
-        // Save Account
-        if($('.Header .Userpic').attr('style')){
-            let account = $('.Header .Userpic').attr('style').split('/')[4];
-            localStorage.setItem(WSTOOLS_ACCOUNT, account);
-        }else{
-            localStorage.setItem(WSTOOLS_ACCOUNT, null);
+      // Save Account
+      if ($('.Header .Userpic').attr('style')) {
+        let account = $('.Header .Userpic')
+          .attr('style')
+          .split('/')[4];
+        localStorage.setItem(WSTOOLS_ACCOUNT, account);
+      } else {
+        localStorage.setItem(WSTOOLS_ACCOUNT, null);
+      }
+
+      // Tag Filter Scheduler(3 sec)
+      setInterval(() => {
+        console.log('Run tag filter..');
+
+        const tagFilterList = JSON.parse(
+          localStorage.getItem(WSTOOLS_POST_FILTER_KEY),
+        );
+
+        if (!tagFilterList) return;
+        if (
+          $('#posts_list ul li:eq(0)')
+            .find('.timestamp__link')
+            .attr('href') == undefined
+        )
+          return;
+
+        // 최초 아이템은 따로 삭제 해준다.
+        if (
+          tagFilterList.indexOf(
+            $('#posts_list ul li:eq(0)')
+              .find('.timestamp__link')
+              .attr('href')
+              .split('/')[1],
+          ) > -1
+        ) {
+          $('#posts_list ul li:eq(0)').remove();
         }
-        
-        // Tag Filter Scheduler(3 sec)
-        setInterval(() => {
-            console.log('Run tag filter..');
 
-            const tagFilterList = JSON.parse(localStorage.getItem(WSTOOLS_POST_FILTER_KEY));
+        $('#posts_list ul li:eq(0)')
+          .siblings()
+          .each((idx, item) => {
+            if (item.querySelector('.articles__content-block--ad'))
+              item.querySelector('.articles__content-block--ad').remove();
 
-            if(!tagFilterList) return;
-            if ($('#posts_list ul li:eq(0)').find('.timestamp__link').attr('href') == undefined) return;
-
-            // 최초 아이템은 따로 삭제 해준다.
-            if (tagFilterList.indexOf($('#posts_list ul li:eq(0)').find('.timestamp__link').attr('href').split('/')[1]) > -1) {
-                $('#posts_list ul li:eq(0)').remove();
+            if (
+              tagFilterList.indexOf(
+                $(item)
+                  .find('.timestamp__link')
+                  .attr('href')
+                  .split('/')[1],
+              ) > -1
+            ) {
+              $(item).remove();
             }
+          });
+      }, 3000);
 
-            $('#posts_list ul li:eq(0)').siblings().each((idx, item) => {
-                if (item.querySelector('.articles__content-block--ad')) item.querySelector('.articles__content-block--ad').remove();
+      // 보팅 파워 가져오기
+      let account = localStorage.getItem(WSTOOLS_ACCOUNT);
+      if (account) {
+        chrome.runtime.sendMessage({
+          action: 'getAccount',
+          data: { username: account },
+        });
+      }
 
-                if (tagFilterList.indexOf($(item).find('.timestamp__link').attr('href').split('/')[1]) > -1) {
-                    $(item).remove();
-                }
-            });
-        }, 3000);
+      let self = this;
 
-        // var f = $('#scotAccountInfo').contents();
-        // scotAccountInfo
+      chrome.extension.onMessage.addListener(function(request) {
+        console.log('getAccount', request);
+        const { action, data } = request;
+        if (action === 'getAccount') {
+          self.steemVP = data.steem;
+          self.sctVP = data.scot;
+        }
+      });
     },
     methods: {
-        allClose() {
-            this.showUserShortcut = false;
-            this.showTagShortcut = false;
-            this.showTagFilter = false;
-            this.showGetMarkdown = false;
-            this.showFamilySite = false;
-            this.showRunUrl = false;
-        },
-        showMenu() {
-            this.show = !this.show;
-            if (this.show) {
-                this.menuIcon = 'cancel';
-            } else {
-                this.menuIcon = 'menu';
-            }
-        },
-        goCategory() {
-            alert('멋진 모습으로 개발중..!!');
-            // window.open(`https://anpigon.github.io/steemit-community/#/`);
-        },
-        userShortcut() {
-            this.allClose();
-            this.showUserShortcut = true;
-        },
-        tagFilter() {
-            this.allClose();
-            this.showTagFilter = true;
-        },
-        tagShortcut() {
-            this.allClose();
-            this.showTagShortcut = true;
-        },
-        getMarkdown() {
-            this.allClose();
-            this.showGetMarkdown = true;
-        },
-        goFamilySite() {
-            this.allClose();
-            this.showFamilySite = true;
-        },
-        closeShortcutUser() {
-            this.showUserShortcut = false;
-        },
-        runUrl() {
-            this.allClose();
-            this.showRunUrl = true;
+      allClose() {
+        this.showUserShortcut = false;
+        this.showTagShortcut = false;
+        this.showTagFilter = false;
+        this.showGetMarkdown = false;
+        this.showFamilySite = false;
+        this.showRunUrl = false;
+      },
+      showMenu() {
+        this.show = !this.show;
+        if (this.show) {
+          this.menuIcon = 'cancel';
+        } else {
+          this.menuIcon = 'menu';
         }
-    }
-}))()
+      },
+      goCategory() {
+        alert('멋진 모습으로 개발중..!!');
+        // window.open(`https://anpigon.github.io/steemit-community/#/`);
+      },
+      userShortcut() {
+        this.allClose();
+        this.showUserShortcut = true;
+      },
+      tagFilter() {
+        this.allClose();
+        this.showTagFilter = true;
+      },
+      tagShortcut() {
+        this.allClose();
+        this.showTagShortcut = true;
+      },
+      getMarkdown() {
+        this.allClose();
+        this.showGetMarkdown = true;
+      },
+      goFamilySite() {
+        this.allClose();
+        this.showFamilySite = true;
+      },
+      closeShortcutUser() {
+        this.showUserShortcut = false;
+      },
+      runUrl() {
+        this.allClose();
+        this.showRunUrl = true;
+      },
+    },
+  }),
+)();
