19849706dcf9130a7541ddf90b0419cd4c7a6b0f
markan <markan@nexl.kr>
2019-06-03 11:17:27 +0900

    보팅 금액 & 스팀 파워 출력

--- background/account.js
+++ background/account.js
@@ -1,5 +1,6 @@
 const VP_REGENERATION_SECS_STEEM = 432000;
 const VP_REGENERATION_SECS_SCT = 216000;
+const VP_REGENERATION_SECS_SCOT_MAP = {};
 
 const getAccount = username => {
   return fetch('https://api.steemit.com/', {
@@ -28,12 +29,85 @@ const currentVotinPower = ({ last_vote_time, voting_power, symbol }) => {
     // symbol이 없으면 steem
     regenerationSec = VP_REGENERATION_SECS_STEEM;
   } else if (symbol === 'SCT') {
-    // SCT는 절반으로 줄임
-    regenerationSec = VP_REGENERATION_SECS_SCT;
+    // SCT는 절반으로 줄임(네트워크에서 정보를 가져오지만 못가져올 경우를 대비해서 디폴트값을 셋팅)
+    regenerationSec =
+      VP_REGENERATION_SECS_SCOT_MAP['SCT'] || VP_REGENERATION_SECS_SCT;
   } else {
     // 그외 scot는 아직 미정
-    regenerationSec = VP_REGENERATION_SECS_STEEM;
+    regenerationSec =
+      VP_REGENERATION_SECS_SCOT_MAP[symbol] || VP_REGENERATION_SECS_STEEM;
   }
   const vp = voting_power + (10000 * elapsed) / regenerationSec;
   return Math.min(vp / 100, 100).toFixed(2);
+};
+
+fetch('https://scot-api.steem-engine.com/config')
+  .then(r => r.json())
+  .then(results => {
+    for (result of results) {
+      VP_REGENERATION_SECS_SCOT_MAP[result.token] =
+        result.vote_regeneration_seconds;
+    }
+    console.log(
+      'VP_REGENERATION_SECS_SCOT_MAP:',
+      VP_REGENERATION_SECS_SCOT_MAP,
+    );
+  });
+
+// 업보팅 금액 계산하기
+const calculatorUpvoteValue = (
+  global,
+  price,
+  rewardFund,
+  account,
+  votePower,
+  voteWeight = 100,
+) => {
+  const steemVp = votePower || currentVotinPower(account);
+  let rate = parseInt((parseFloat(steemVp) * 100 * (voteWeight * 100)) / 1e4);
+  rate = parseInt((rate + 49) / 50) * 100; // 변환식(?)
+  const totalVestingFundSteem = parseFloat(global.total_vesting_fund_steem);
+  const totalVestingShares = parseFloat(global.total_vesting_shares);
+  const steemPrice = parseFloat(price.base) / parseFloat(price.quote);
+  const rewardBalance = parseFloat(rewardFund.reward_balance);
+  const recentClaims = parseFloat(rewardFund.recent_claims);
+  const vestingSteem = steem.formatter.vestingSteem(account, global);
+  const receivedVestingSteem = steem.formatter.vestToSteem(
+    account.received_vesting_shares,
+    global.total_vesting_shares,
+    global.total_vesting_fund_steem,
+  );
+  const delegatedVestingSteem = steem.formatter.vestToSteem(
+    account.delegated_vesting_shares,
+    global.total_vesting_shares,
+    global.total_vesting_fund_steem,
+  );
+  const totalVestingSteem =
+    vestingSteem + receivedVestingSteem - delegatedVestingSteem;
+  const upvoteValue =
+    (totalVestingSteem / (totalVestingFundSteem / totalVestingShares)) *
+    rate *
+    (rewardBalance / recentClaims) *
+    steemPrice;
+  return upvoteValue;
+};
+
+// 스팀 파워 계산
+const calculatorSteemPower = (global, account) => {
+  const vestingSteem = steem.formatter.vestingSteem(account, global);
+  const receivedVestingSteem = steem.formatter.vestToSteem(
+    account.received_vesting_shares,
+    global.total_vesting_shares,
+    global.total_vesting_fund_steem,
+  );
+  const delegatedVestingSteem = steem.formatter.vestToSteem(
+    account.delegated_vesting_shares,
+    global.total_vesting_shares,
+    global.total_vesting_fund_steem,
+  );
+  return {
+    vestingSteem,
+    receivedVestingSteem,
+    delegatedVestingSteem,
+  };
 };
--- popup/src/get_account.js
+++ popup/src/get_account.js
@@ -1,39 +1,98 @@
 // 보팅 파워 보이기
-const showVotingProgress = (target, value) => {
-  $(target)
-    .progress({
-      percent: parseFloat(value),
-    })
-    .end()
-    .find('.progress')
-    .text(`${value}%`);
+const showVotingProgress = (target, value, symbol) => {
+  $(target).progress({
+    percent: parseFloat(value),
+    text: {
+      percent: `${value}% ${symbol}`,
+    },
+  });
 };
 
 // 계정 정보 가져오기
 const getAccountAllInfo = username => {
   console.log('getAccountAllInfo');
   if (Boolean(username)) {
-    Promise.all([getAccount(username), getSCOTAccount(username)]).then(
-      ([steem, scot]) => {
-        console.log(steem, scot);
-        if (!steem) return;
+    Promise.all([
+      steem.api.getDynamicGlobalPropertiesAsync(),
+      steem.api.getCurrentMedianHistoryPriceAsync(),
+      steem.api.getRewardFundAsync('post'),
+      getAccount(username),
+      getSCOTAccount(username),
+    ]).then(([global, price, rewardFund, account, scot]) => {
+      console.log({ account, scot });
+      if (!account) return;
 
-        const steemVp = currentVotinPower(steem);
-        showVotingProgress('#steem_vp', steemVp);
+      const steemVp = currentVotinPower(account);
+      showVotingProgress('#steem_vp', steemVp, 'STEEM');
 
-        if (scot['SCT']) {
-          const sctVp = currentVotinPower(scot['SCT']);
-          showVotingProgress('#scot_vp', sctVp);
-        }
+      // 보팅파워 남은 재생시간
+      const remainHours =
+        ((10000 - parseFloat(steemVp) * 100) * (5 * 60 * 60 * 24)) /
+        (60 * 60 * 10000);
 
-        chrome.storage.sync.set(
-          {
-            USERNAME: username,
-          },
-          () => console.log('Value is set to ', username),
-        );
-      },
-    );
+      if (scot['SCT']) {
+        const sctVp = currentVotinPower(scot['SCT']);
+        showVotingProgress('#scot_vp', sctVp, 'SCT');
+      }
+
+      if (scot['AAA']) {
+        const aaaVp = currentVotinPower(scot['AAA']);
+        showVotingProgress('#aaa_vp', aaaVp, 'AAA');
+      }
+
+      console.log({ global, price, rewardFund });
+
+      // 명성 계산
+      let reputation = parseInt(account.reputation);
+      reputation =
+        Math.max(Math.log10(Math.abs(reputation)) - 9, 0) *
+          (reputation >= 0 ? 1 : -1) *
+          9 +
+        25;
+      $('#reputation').html(
+        `<b>${reputation.toFixed(3)}</b> (${account.post_count} posts)`,
+      );
+
+      // 스팀파워 계산
+      const sp = calculatorSteemPower(global, account);
+      const steemPowerHtml = [];
+      steemPowerHtml.push(`<b>${sp.vestingSteem.toFixed(2)} STEEM</b>`);
+      if (sp.receivedVestingSteem || sp.delegatedVestingSteem) {
+        steemPowerHtml.push(' (');
+        if (sp.receivedVestingSteem)
+          steemPowerHtml.push(`+${sp.receivedVestingSteem.toFixed(2)}`);
+        if (sp.delegatedVestingSteem)
+          steemPowerHtml.push(` -${sp.delegatedVestingSteem.toFixed(2)}`);
+        steemPowerHtml.push(')');
+      }
+      $('#steemPower').html(steemPowerHtml.join(''));
+
+      // 보팅 가치 계산
+      const upvoteValue = calculatorUpvoteValue(
+        global,
+        price,
+        rewardFund,
+        account,
+      );
+      const mapUpvoteValue = calculatorUpvoteValue(
+        global,
+        price,
+        rewardFund,
+        account,
+        100,
+        100,
+      );
+      $('#upvoteValue').html(
+        `<b>$${upvoteValue.toFixed(3)}</b> ($${mapUpvoteValue.toFixed(3)})`,
+      );
+
+      chrome.storage.sync.set(
+        {
+          USERNAME: username,
+        },
+        () => console.log('Value is set to ', username),
+      );
+    });
   }
 };
 
@@ -50,3 +109,6 @@ chrome.storage.sync.get('USERNAME', ({ USERNAME: username }) => {
   $('#username').val(username);
   getAccountAllInfo(username);
 });
+
+// SCOT 환경 변수 가져오기
+// const ssc = new SSC('https://api.steem-engine.com/rpc/');
